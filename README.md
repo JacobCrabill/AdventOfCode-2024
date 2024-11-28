987cf7f286588644085f93e3830c2871e5c11149
JacobCrabill <jacob.crabill@gmail.com>
2024-11-27 20:12:17 -0800

    Initial commit (template, common, README)

---
+++ README.md
@@ -0,0 +1,61 @@
+# Advent Of Code 2024
+
+Advent Of Code 2024 Solutions in Zig (version 0.13.0)
+
+## Basic Usage
+
+The `template` folder contains, obviously, the template for a single day's problems. The
+`create_day` script will conveniently copy-paste the template to a new day for you, including
+changing the output binary name to match the day.
+
+For example: `create_day 5` creates a folder named `5` which contains a Zig project setup to build a
+`day5` binary.
+
+## Using the templates
+
+Each day is a standalone Zig project, containing a `build.zig` file, a `main.zig` file, and `.txt`
+files for the problem input. The `test.txt` file is intended for the sample input, while the
+`input.txt` file is intended for your unique input.
+
+Within `main.zig` there are placeholder functions for `part1` and `part2`, and a `main()` which runs
+both parts (and helpfully times them for you). There are also two tests, one for each part; you can
+use these to check your solution against the sample input and solution given in the problem
+statements. Just change the line `const answer: usize = 0;` to the expected answer.
+
+The template also creates a `utils` module from the `common` folder in the root; this can be used to
+store generically useful algorithms and data structures to speed up your problem solving. The
+template `main.zig` file imports this module as `utils`.
+
+## Build & Run
+
+```sh
+./create_day 5
+cd 5
+zig build test # Build and run the tests
+zig build run  # Build and run main()
+```
+
+Sample output for unit tests:
+
+```sh
+AdventOfCode2023/5$ zig build test --summary all
+test
+└─ run test stderr
+[AoC] (warn):  -- Running Tests --
+[AoC] (warn): [Test] Part 1: 0
+[AoC] (warn): [Test] Part 2: 0
+Build Summary: 3/3 steps succeeded; 2/2 tests passed
+test success
+└─ run test 2 passed 737us MaxRSS:1M
+   └─ zig test Debug native cached 6ms MaxRSS:37M
+```
+
+Sample output for for the full input:
+
+```sh
+AdventOfCode2023/5$ zig build run
+info(aoc): Part 1 answer: << 0 >>
+info(aoc): Part 1 took 0.000010s
+info(aoc): Part 2 answer: << 0 >>
+info(aoc): Part 2 took 0.000004s
+```
---
+++ common/utils.zig
@@ -0,0 +1,254 @@
+const std = @import("std");
+
+const TokenIterator = std.mem.TokenIterator(u8, .any);
+const SplitIterator = std.mem.SplitIterator(u8, .sequence);
+
+/// Simplified wrapper around Heap sort
+pub fn heapSort(T: anytype, items: []T, lessThan: fn (void, T, T) bool) void {
+    std.sort.heap(T, items, {}, lessThan);
+}
+
+/// Count occurances of 'value' within 'haystack'
+pub fn countScalar(T: anytype, haystack: []const T, value: T) usize {
+    return std.mem.count(T, haystack, &[1]T{value});
+}
+
+/// Split the input by newline chars "\n"
+/// Note: This keeps empty lines
+pub fn lines(input: []const u8) SplitIterator {
+    return std.mem.split(u8, input, "\n");
+}
+
+pub fn lineCount(input: []const u8) usize {
+    var line_iter = lines(input);
+    var count: usize = 0;
+    while (line_iter.next()) |line| {
+        if (line.len == 0) break;
+        count += 1;
+    }
+    return count;
+}
+
+pub fn split(input: []const u8, delim: []const u8) SplitIterator {
+    return std.mem.splitSequence(u8, input, delim);
+}
+
+/// Return the first item of the split
+pub fn splitFirst(input: []const u8, delim: []const u8) ?[]const u8 {
+    var iter = split(input, delim);
+    return iter.next();
+}
+
+/// Return the last item of the split
+pub fn splitLast(input: []const u8, delim: []const u8) ?[]const u8 {
+    var last: ?[]const u8 = null;
+    var iter = split(input, delim);
+    while (iter.next()) |item| {
+        last = item;
+    }
+    return last;
+}
+
+/// Return the Nth item of the split
+pub fn splitN(input: []const u8, delim: []const u8, idx: usize) ?[]const u8 {
+    var last: ?[]const u8 = null;
+    var iter = split(input, delim);
+    var i: usize = 0;
+    while (iter.next()) |item| {
+        if (i > idx) break;
+        last = item;
+        i += 1;
+    }
+    return last;
+}
+
+/// Split the input by any of the chars in 'delim', skipping empty separators
+pub fn tokenize(input: []const u8, delim: []const u8) TokenIterator {
+    return std.mem.tokenizeAny(u8, input, delim);
+}
+
+/// Return the first item of the tokenization
+pub fn tokenizeFirst(input: []const u8, delim: []const u8) ?[]const u8 {
+    var iter = tokenize(input, delim);
+    return iter.next();
+}
+
+/// Return the last item of the tokenization
+pub fn tokenizeLast(input: []const u8, delim: []const u8) ?[]const u8 {
+    var last: ?[]const u8 = null;
+    var iter = tokenize(input, delim);
+    while (iter.next()) |item| {
+        last = item;
+    }
+    return last;
+}
+
+/// Return the Nth item of the tokenization
+pub fn tokenizeN(input: []const u8, delim: []const u8, idx: usize) ?[]const u8 {
+    var last: ?[]const u8 = null;
+    var iter = tokenize(input, delim);
+    var i: usize = 0;
+    while (iter.next()) |item| {
+        if (i > idx) break;
+        last = item;
+        i += 1;
+    }
+    return last;
+}
+
+// Convert timer value to seconds (float)
+pub fn ns2sec(nanos: u64) f64 {
+    return @as(f64, @floatFromInt(nanos)) / 1.0e9;
+}
+
+/// Start and return a simple Timer
+pub fn Timer() !std.time.Timer {
+    return try std.time.Timer.start();
+}
+
+// Simple wrapper around std.io.getStdOut
+pub fn stdout(comptime fmt: []const u8, args: anytype) void {
+    const out = std.io.getStdOut().writer();
+    out.print(fmt, args) catch @panic("stdout failed!");
+}
+
+pub fn Math(comptime T: type) type {
+    return struct {
+        pub fn gcd(m_: T, n_: T) T {
+            var m = m_;
+            var n = n_;
+            var tmp: T = 0;
+            while (m > 0) {
+                tmp = m;
+                m = @mod(n, m);
+                n = tmp;
+            }
+            return n;
+        }
+
+        pub fn lcm(a: T, b: T) T {
+            return @divFloor(a, gcd(a, b)) * b;
+        }
+    };
+}
+
+test "LCM" {
+    const math = Math(usize);
+    const lcm: usize = 36;
+    try std.testing.expectEqual(lcm, math.lcm(12, 18));
+    try std.testing.expectEqual(lcm, math.lcm(18, 12));
+}
+
+// Classic Set container type, like C++'s std::undordered_set
+pub fn Set(comptime keytype: type) type {
+    return struct {
+        const Self = @This();
+        const Key = keytype;
+        const MapType = std.AutoHashMap(keytype, void);
+        const Size = MapType.Size;
+        map: MapType,
+        alloc: std.mem.Allocator,
+
+        pub fn init(alloc: std.mem.Allocator) Self {
+            return Self{
+                .alloc = alloc,
+                .map = MapType.init(alloc),
+            };
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.map.deinit();
+        }
+
+        pub fn count(self: *Self) Size {
+            return self.map.count();
+        }
+
+        pub fn capacity(self: *Self) Size {
+            return self.map.capacity();
+        }
+
+        pub fn getOrPut(self: *Self, key: Key) !void {
+            try self.map.getOrPut(key, {});
+        }
+
+        pub fn put(self: *Self, key: Key) !void {
+            try self.map.put(key, {});
+        }
+
+        pub fn putNoClobber(self: *Self, key: Key) !void {
+            try self.map.putNoClobber(key, {});
+        }
+
+        pub fn contains(self: *Self, key: Key) bool {
+            return self.map.contains(key);
+        }
+
+        pub fn remove(self: *Self, key: Key) bool {
+            return self.map.remove(key);
+        }
+
+        pub fn iterator(self: *Self) MapType.Iterator {
+            return self.map.iterator();
+        }
+
+        // Alias for remove
+        pub fn pop(self: *Self, key: Key) bool {
+            return self.remove(key);
+        }
+    };
+}
+
+test "AutoHashMap set test" {
+    var set = Set(u8).init(std.testing.allocator);
+    defer set.deinit();
+
+    try set.put(10);
+    try set.put(50);
+    try set.put(8);
+
+    try std.testing.expect(set.count() == 3);
+    try std.testing.expect(set.capacity() == 8);
+    try std.testing.expect(set.contains(8));
+    try std.testing.expect(set.contains(10));
+    try std.testing.expect(set.contains(1) == false);
+
+    try std.testing.expect(set.pop(10) == true);
+    try std.testing.expect(set.pop(10) == false);
+    try std.testing.expect(set.count() == 2);
+}
+
+test "split First/Last/N" {
+    const input = "abc, 123, def";
+
+    try std.testing.expectEqualSlices(u8, "def", splitLast(input, ", ").?);
+    try std.testing.expectEqualSlices(u8, "abc", splitFirst(input, ", ").?);
+    try std.testing.expectEqualSlices(u8, "123", splitN(input, ", ", 1).?);
+}
+
+test "split uses full delim and keeps empties" {
+    const input = "abc,123, , def";
+    const delim = ", ";
+
+    try std.testing.expectEqualSlices(u8, "def", splitLast(input, delim).?);
+    try std.testing.expectEqualSlices(u8, "abc,123", splitFirst(input, delim).?);
+    try std.testing.expectEqualSlices(u8, "", splitN(input, delim, 1).?);
+}
+
+test "tokenize First/Last/N" {
+    const input = "abc, 123, def";
+
+    try std.testing.expectEqualSlices(u8, "def", tokenizeLast(input, ", ").?);
+    try std.testing.expectEqualSlices(u8, "abc", tokenizeFirst(input, ", ").?);
+    try std.testing.expectEqualSlices(u8, "123", tokenizeN(input, ", ", 1).?);
+}
+
+test "tokenize uses any delim and skips empties" {
+    const input = "abc,123, , def";
+    const delim = ", ";
+
+    try std.testing.expectEqualSlices(u8, "def", tokenizeLast(input, delim).?);
+    try std.testing.expectEqualSlices(u8, "abc", tokenizeFirst(input, delim).?);
+    try std.testing.expectEqualSlices(u8, "123", tokenizeN(input, delim, 1).?);
+    try std.testing.expectEqualSlices(u8, "def", tokenizeN(input, delim, 2).?);
+}
